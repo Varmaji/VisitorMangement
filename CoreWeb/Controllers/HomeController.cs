8fc470a30a03315b8461c59122749b5fc2eab724
Varmaji <92varma@gmail.com>
2020-06-23 19:43:46 +0530

    commit

---
+++ CoreWeb/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CoreWeb.Models;
+
+namespace CoreWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            TimeModel time = new TimeModel();
+
+            time.Time = DateTime.Now.ToString();
+            return View(time);
+        }
+
+    }
+}
---
+++ CoreWeb/Models/githubController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWeb.Models
+{
+    public class githubController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string githubApi()
+        {
+
+            return "";
+        }
+    }
+}
